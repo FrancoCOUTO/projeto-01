f906a3faf7e2861cd408d4e871ff8c44cc575278
Franco Couto <francocouto@gmail.com>
2026-05-20 18:13:34 -0300

    Fix: Corrige versao do Spring Boot e propriedades do H2

--- src/main/java/com/ppfranco/projeto01/entidades/User.java
+++ src/main/java/com/ppfranco/projeto01/entidades/User.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 
 
+@Entity
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
